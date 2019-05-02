2f659374357cc99c72292a24fa2a693021cc3e2e
Jun Tan <tanjunyuan@hotmail.co.uk>
2019-05-02 08:34:33 -0500

    partial arrow

--- csv_to_arrow.py
+++ csv_to_arrow.py
@@ -6,11 +6,13 @@ if __name__ == "__main__":
     CSV_NAME = "test.csv"
     # make sure bools are read correctly
     options = csv.ConvertOptions(
-            column_types = {
+            column_types = 
+            {
+                "i64": pa.int64(),
                 "bool": pa.bool_()
             })
     table = csv.read_csv(CSV_NAME, convert_options = options)
     # write table to arrow
-    writer = pa.RecordBatchFileWriter("test.arrow", table.schema)
+    writer = pa.RecordBatchFileWriter("test2.arrow", table.schema)
     writer.write_table(table)
     writer.close()
